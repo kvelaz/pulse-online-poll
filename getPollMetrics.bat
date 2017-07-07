b2cb85a46db89571a4d8c7a9b73fb992c212598a
kvelaz <enrique.velazquez.martin@gmail.com>
2017-07-07 14:15:14 +0200

    v20

--- getPollMetrics.bat
+++ getPollMetrics.bat
@@ -1,11 +1,11 @@
-rem @echo off
+@echo off
 
 SET PATH=%PATH%;c:\windows\system32
 :loop
 
-echo POLL_QUESTION_1 50  EM-EVELAZQU-W1
-echo POLL_QUESTION_2 33  
-echo POLL_QUESTION_3 75 
+echo POLL_QUESTION_1 50 >CON
+echo POLL_QUESTION_2 33 >CON
+echo POLL_QUESTION_3 75 >CON
 timeout /t 1 > nul
 
 goto loop
