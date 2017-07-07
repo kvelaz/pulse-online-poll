3f500ef2a4cc9570e47b9c7acc62849f42a9f588
kvelaz <enrique.velazquez.martin@gmail.com>
2017-07-07 12:54:02 +0200

    v12
    v12

--- getPollMetrics.bat
+++ getPollMetrics.bat
@@ -3,9 +3,9 @@
 
 :loop
 
-echo POLL_QUESTION_1 50 Online_poll 
-echo POLL_QUESTION_2 33 Online_poll 
-echo POLL_QUESTION_3 75 Online_poll 
+echo POLL_QUESTION_1 50  
+echo POLL_QUESTION_2 33  
+echo POLL_QUESTION_3 75 
 rem timeout /t 1 > nul
 
 goto loop
