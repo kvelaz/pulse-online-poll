21fcd2c5a225e2e507a36bddca386aa892e15ca0
kvelaz <enrique.velazquez.martin@gmail.com>
2017-07-24 16:12:10 +0200

    v30

--- getPollMetrics.bat
+++ getPollMetrics.bat
@@ -1,6 +1,7 @@
 @echo off
 
-SET PATH=%PATH%;c:\windows\system32;C:\Program Files\Java\jdk1.8.0_121\bin
+SET PATH=%PATH%;c:\windows\system32;C:\Program Files\Java\jdk1.8.0_121\bin;C:\Users\evelazqu\Desktop\install
+
 rem :loop
 
 rem echo POLL_QUESTION_1 50
@@ -10,4 +11,11 @@ rem rem timeout /t 1 >nul
 
 rem goto loop
 
-java com.bmc.pulsemetrics.GetOnlinePollMetrics
+rem java com.bmc.pulsemetrics.GetOnlinePollMetrics
+
+
+rem curl -k -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Basic ZW5yaXF1ZV92ZWxhenF1ZXpAYm1jLmNvbTpwNHJyNHI0" "https://www.polleverywhere.com/multiple_choice_polls/dRaAvEw17pm1tCn/results" > c:\pollplugin\poll.json
+
+
+java -classpath PollMetrics.jar;json-simple-1.1.1.jar;commons-logging-1.2.jar;httpclient-4.5.2.jar;httpcore-4.4.4.jar com.bmc.pulsemetrics.GetOnlinePollMetrics
+
